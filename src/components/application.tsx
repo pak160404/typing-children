12dde30c98606343c414c6e0eed7dfeb44ea8f97
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-08-12 23:35:29 +0700

    Learning

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,3 +1,5 @@
+import { PropsWithChildren } from 'react';
+
 /**
  * Things you could try:
  *
@@ -7,14 +9,19 @@
  * React.ReactChildren;
  * React.ReactChild[];
  */
+//most of the time use ReactNode
+  //type BoxProps = { children: JSX.Element[] /* 👈 Get rid of this! */ }; - use with one children, and children required
 
-type BoxProps = { children: any /* 👈 Get rid of this! */ };
-
-const Box = ({ children }: BoxProps) => {
+  //PropsWithChildren default ReactNode? - Use most of the time to pass props, null children
+type BoxProps= PropsWithChildren<{
+  style?: React.CSSProperties; //type safety
+}>
+const Box = ({ children,style }: BoxProps) => {
   return (
     <section
       className="m-4"
-      style={{ padding: '1em', border: '5px solid purple' }}
+      //style={{ padding: '1em', border: '5px solid purple'}} - fixed style unchangeable
+      style={{ padding: '1em', border: '5px solid purple' ,...style }} // can merge/add/overiding default style by passing
     >
       {children}
     </section>
@@ -24,7 +31,7 @@ const Box = ({ children }: BoxProps) => {
 const Application = () => {
   return (
     <main className="m-8">
-      <Box>
+      <Box style={{ padding: '1em', backgroundColor: 'blue' }}> {/*example for passing style*/}
         Just a string.
         <p>Some HTML that is not nested.</p>
         <Box>
